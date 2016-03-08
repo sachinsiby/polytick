c3c2a200ba4d6417e67aea9dc3a6155b28cdc232
Sachin Siby <sachinsiby@gmail.com>
2016-03-08 00:24:21 -0500

    Disable assets precompile

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ module Polytick
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.api_only = true
+    config.api_only         = true
+    config.assets.enabled   = false
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
@@ -23,5 +24,6 @@ module Polytick
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
   end
 end
