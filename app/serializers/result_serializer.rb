049c1b17df16c6ce5a054cab5ec5b27bd54fc59d
Sachin Siby <sachinsiby@gmail.com>
2016-04-03 23:50:00 -0400

    Sort by percentage

--- app/serializers/result_serializer.rb
+++ app/serializers/result_serializer.rb
@@ -3,6 +3,6 @@ class ResultSerializer < ActiveModel::Serializer
   attributes :result_statistics#, each_serializer: ResultStatisticSerializer
 
   def result_statistics
-    object.result_statistics.group_and_sum.as_json.map{ |i| i.except("id") }
+    object.result_statistics.group_and_sum.as_json.map{ |i| i.except("id") }.sort_by{|obj| -obj["percentage"] }
   end
 end
